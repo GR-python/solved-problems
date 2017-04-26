9be19c2c4a1e9b4aba151d4356ac8212c011b260
k33theod <k33theod@gmail.com>
2017-04-26 18:10:34 +0200

    Update askisi2.py

--- askisi2.py
+++ askisi2.py
@@ -1,12 +1,11 @@
 """
-2. Να γράψετε πρόγραμμα σε γλώσσα Python, το οποίο να υπολογίζει και να εκτυπώνει το άθροισμα 0+00+000+…+000000000 (όπου Τ το τελευταίο ψηφίο του ΑΜ σας).
+2. Να γράψετε πρόγραμμα σε γλώσσα Python, το οποίο να υπολογίζει και να εκτυπώνει το άθροισμα
+T+TT+TTT+…+TTTTTTTTT(9 φορές) (όπου Τ το τελευταίο ψηφίο του ΑΜ σας).
 """
 
-def askisi2(afm_last_digit):
-  if  isinstance(afm_last_digit,str) and len (afm_last_digit)==1 and afm_last_digit.isdigit():
-    result=0
-    for i in range(1,10):
-      result+=int((afm_last_digit)*i)
-    return result
-  else:
-    print('To function argument prepei na einai sting me 1 psifio' )
+def askisi2(afm_last_digit):#To function argument prepei na einai sting me 1 psifio
+  result=0
+  for i in range(1,10):
+    result+=int((afm_last_digit)*i)
+  return result
+  
