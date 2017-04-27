bf7202cb872f6e581b06215fb2b5aa59ed244f22
k33theod <k33theod@gmail.com>
2017-04-27 21:12:35 +0200

    Update askisi2.py

--- askisi2.py
+++ askisi2.py
@@ -1,14 +1,17 @@
 """
-2. Να γράψετε πρόγραμμα σε γλώσσα Python, το οποίο να υπολογίζει και να εκτυπώνει το άθροισμα
+Να γράψετε πρόγραμμα, το οποίο να υπολογίζει και να εκτυπώνει το άθροισμα
 T+TT+TTT+…+TTTTTTTTT(9 φορές) (όπου Τ το τελευταίο ψηφίο του ΑΜ σας).
 """
 
 def askisi2(afm_last_digit):
-  afm_last_digit_str=str(afm_last_digit)
-  result=0
-  for i in range(1,10):
-    result+=int((afm_last_digit_str)*i)
-  return result
+  if afm_last_digit in range(10):
+    afm_last_digit_str=str(afm_last_digit)
+    result=0
+    for i in range(1,10):
+      result+=int((afm_last_digit_str)*i)#Δημιουργούμαι κάθε φορά το T[1-9] ως str και μετά το μετατρέπω σε int 
+    return result
+  else:
+    return f'To {afm_last_digit} πρέπει να είναι αριθμός 0-9'
   
 def askisi(a):
   result=0
@@ -16,7 +19,9 @@ def askisi(a):
     number=0
     j=0
     while j<i:
-      number=number+a*10**j
+      number=number+a*10**j#Δημιουργούμαι κάθε φορά τον ακέραιο πχ 22222(5 φορές το 2) ώς 2+2*10**1+2*10**2+2*10**3+2*10**4
       j+=1
     result+=number
   return result
+#Στις δύο παραπάνω λύσεις μου δεν χρησιμοποιώ το print(result) που ζητάει η άσκηση αλλά το return result.
+#Έλεγχο εισόδου του argument μπορούμε να επαναλάβουμε το ίδιο και στην δεύτερη άσκηση.
