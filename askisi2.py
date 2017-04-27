9b8685747c49904e7aef772ead7d48206c388ab7
k33theod <k33theod@gmail.com>
2017-04-27 10:28:07 +0200

    Update askisi2.py

--- askisi2.py
+++ askisi2.py
@@ -3,9 +3,20 @@
 T+TT+TTT+…+TTTTTTTTT(9 φορές) (όπου Τ το τελευταίο ψηφίο του ΑΜ σας).
 """
 
-def askisi2(afm_last_digit):#To function argument prepei na einai sting me 1 psifio
+def askisi2(afm_last_digit):
+  afm_last_digit_str=str(afm_last_digit)
   result=0
   for i in range(1,10):
-    result+=int((afm_last_digit)*i)
+    result+=int((afm_last_digit_str)*i)
   return result
   
+def askisi(a):
+  result=0
+  for i in range(1,10):
+    number=0
+    j=0
+    while j<i:
+      number=number+a*10**j
+      j+=1
+    result+=number
+  return result
